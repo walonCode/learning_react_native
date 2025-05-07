5eac5c989702fc0fe4f65e2f004223afa17b2bc7
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-05-07 22:31:57 +0000

    add and learnt thedifference between elevation and shadow for ios and andriod

--- lesson_01/app/index.tsx
+++ lesson_01/app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, TextInput, Pressable, Alert } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, Pressable, Alert, ScrollView, Platform } from 'react-native'
 import React, { useState } from 'react'
 import { Ionicons } from "@expo/vector-icons"
 import PokemonCard from '@/components/PokemonCard'
@@ -22,31 +22,46 @@ export default function Index() {
     }
   }
   return (
-    <SafeAreaView className='flex-1 flex-col pt-16 items-center'>
-        <View>
-          <Text className='font-bold text-3xl mb-2'>
-            Search from a Pokemon
-          </Text>
-        </View>
-        <View className='flex-row items-center gap-2 mt-2 mb-2'> 
-            <TextInput
-            value={searchTerm}
-            onChangeText={setSearchTerm}
-            placeholder='Enter pokemnon name'
-            secureTextEntry={false}
-            autoCapitalize="none"
-            autoCorrect={false}
-            className='border-2 border-black w-[300]'
-            />
-          <Pressable onPress={() => {handleSearch()}}>
-            <View>
-              <Ionicons name="search" color="black" size={30}/>
-            </View>
-          </Pressable>
-        </View>
+  <ScrollView className='bg-white'>
+      <SafeAreaView className='flex-1 flex-col pt-16 items-center'>
+      <View>
+        <Text className='font-bold text-3xl mb-2'>
+          Search from a Pokemon
+        </Text>
+      </View>
+      <View className='flex-row items-center gap-2 mt-2 mb-2'> 
+          <TextInput
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+          placeholder='Enter pokemnon name'
+          secureTextEntry={false}
+          autoCapitalize="none"
+          autoCorrect={false}
+          className='border-2 border-black w-[340] rounded-full px-2'
+          style={{
+            ...Platform.select({
+              ios:{
+                shadowOffset:{width:10, height:10},
+                shadowColor:"white",
+                shadowOpacity: 0.4,
+                shadowRadius:5
+              },
+              android:{
+                elevation:20,
+              }
+            })
+          }}
+          />
+        <Pressable onPress={() => {handleSearch()}}>
+          <View>
+            <Ionicons name="search" color="black" size={30}/>
+          </View>
+        </Pressable>
+      </View>
       <View className='mt-20'>
         <PokemonCard data={data}/>
       </View>
     </SafeAreaView>
+  </ScrollView>
   )
 }
--- lesson_01/components/PokemonCard.tsx
+++ lesson_01/components/PokemonCard.tsx
@@ -1,26 +1,68 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet, Platform, ScrollView } from 'react-native'
 import React from 'react'
 
 export default function PokemonCard({data}:{data:any}) {
   return (
-    <View className='border-2  w-[350] h-[400] px-2 shadow-slate-200 '>
+   <ScrollView>
+     <View style={style.card}>
       <View className='flex-row justify-between items-center mt-5 mb-2 mx-2 '>
-        <Text className='font-bold text-xl' >{data.name}</Text>
+        <Text className='font-bold text-xl capitalize' >{data?.name}</Text>
         <Text className='font-thin border-2 py-2 px-2 rounded-full border-black/50'>Pokemon Experience: {data.base_experience} </Text>
       </View>
       <View className='items-center my-4'>
-        <Image source={{ uri:`${data?.sprites?.front_default}`}} height={100} width={100}/>
+        <Image source={{ uri:`${data?.sprites?.front_default}`}} height={150} width={120}/>
       </View>
       <View className='flex-row items-center justify-between mx-5'>
-        <Text className='font-bold text-2xl'>weight: {data.weight}</Text>
+        <Text className='font-bold text-2xl'>Weight: {data.weight}</Text>
         <View className='flex-col items-center '>
           {data?.abilities?.map((ability:any, index:number) => (
-            <Text key={index}> 
-              Ability: {ability?.name}
+            <Text key={index} className='capitalize text-black'> 
+              Ability: {ability?.ability?.name}
             </Text>
           ))}
         </View>
       </View>
+      <View>
+        {data?.moves?.slice(0,5)?.map((move:any, index:number) => (
+          <Text key={index} className="capitalize">
+            Moves: {move?.move?.name}
+          </Text>
+        ))}
+      </View>
+      <View className='mt-2 '>
+        {data?.types?.map((type:any,index:any) => (
+          <Text key={index} className='font-bold text-[15px] capitalize'>
+            Type: {type?.type?.name}
+          </Text>
+        ))}
+      </View>
     </View>
+   </ScrollView>
   )
 }
+
+const style = StyleSheet.create({
+  card:{
+    borderWidth:2,
+    backgroundColor:'white',
+    borderRadius:16,
+    width:350,
+    height:450,
+    padding:16,
+    margin:16,
+    ...Platform.select({
+      ios:{
+        shadowOffset:{width:10, height:10},
+        shadowColor:"white",
+        shadowOpacity: 0.4,
+        shadowRadius:5
+      },
+      android:{
+        elevation:20
+      }
+    })
+  }
+})
+
+//for shadows in ios use the shadow properties
+//for android use the evelation properties
