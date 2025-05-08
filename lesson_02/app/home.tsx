8c2dd655e97d118e496b4db9076ed1790468b439
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-05-08 23:09:08 +0000

    add a home page that make use of a flatlist

---
+++ lesson_02/app/home.tsx
@@ -0,0 +1,129 @@
+import { View, Text, Alert, FlatList, ActivityIndicator, StyleSheet } from 'react-native'
+import { useEffect, useState } from 'react'
+import axios from "axios"
+
+export default function Home() {
+  const [data, setData] = useState([])
+  const [isRefreshing, setIsRefreshing] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchData = async(limit = 10) => {
+    try{
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/?_limit=${limit}`)
+        if (response.status === 200){
+          console.log(response.data)
+          setData(response.data)
+          setIsLoading(false)
+        }
+    }catch(error){
+      console.log(error)
+      Alert.alert("error fetching the posts")
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  const handleRefresh = async(limit:any) => {
+    setIsRefreshing(true)
+    await fetchData(limit)
+  }
+
+  if(isLoading){
+    return(
+      <View style={{
+        flex:1,
+        alignContent:"center",
+        justifyContent:"center"
+      }}>
+        <ActivityIndicator
+        size="large"
+        color="blue"
+        />
+        <Text style={{
+          color:"blue", textAlign:"center"
+        }}>Loading....</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={{
+      flex:1,
+      alignItems:"center",
+      justifyContent:"center"
+    }}>
+      <FlatList
+      data={data}
+      keyExtractor={({item}:{item:any}) => item?.id}
+      renderItem={({ item }:{item:any}) => {
+        return (
+          <View style={style.card}>
+            <Text style={style.title}> {item.title}</Text>
+            <Text style={style.description}> {item.body} </Text>
+          </View>
+        )
+      }}
+      ListEmptyComponent={() => {
+        return(
+          <View style={{
+            flex:1,
+            alignItems:"center",
+            justifyContent:"center"
+          }}>
+            <Text>
+              Post not found
+            </Text>
+          </View>
+        )
+      }}
+      ItemSeparatorComponent={() => {
+        return(
+          <View style={{
+            marginBottom:10
+          }}></View>
+        )
+      }}
+      ListHeaderComponent={() => {
+        return(
+          <Text style={{
+            fontSize:20,
+            textAlign:"center",
+            marginBottom:10,
+            fontWeight:"500"
+          }}>
+            Posts
+          </Text>
+        )
+      }}
+      ListFooterComponent={() => (
+        <Text style={{ textAlign:"center", marginTop:10, fontWeight:"bold", fontSize:20}}>
+          End of Post list
+        </Text>
+      )}
+      onRefresh={() => handleRefresh(20)}
+      refreshing={isRefreshing}
+      />
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  card:{
+    borderWidth:1,
+    height:250,
+    width:350,
+    padding:10
+  },
+  title:{
+    fontSize:20,
+    fontWeight:"bold",
+    textAlign:"center",
+    marginBottom:5
+  },
+  description:{
+    fontSize: 16,
+    fontWeight:"400",
+  }
+})
--- lesson_02/app/index.tsx
+++ lesson_02/app/index.tsx
@@ -1,15 +1,145 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Button,
+  TextInput,
+  Platform,
+  StatusBar,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useState } from "react";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({
+    name: "",
+    error: "",
+  });
+
+  const router = useRouter()
+
+  const validateForm = () => {
+    let error = { name: "", error: "" };
+
+    if (password.length === 0) {
+      error = { name: "Password", error: "Invalid Password length" };
+    }
+    if (email.length === 0) {
+      error = { name: "Email", error: "Invalid Email length" };
+    }
+    setError(error);
+    return error.name === "";
+  };
+
+  const handleSubmit = () => {
+    if (validateForm()) {
+      console.log("Data:", password, email);
+      router.push('/home')
+      setError({ name: "", error: "" });
+      setEmail("");
+      setPassword("");
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : undefined}
+            style={styles.innerContainer}
+            keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+          >
+            <Text style={styles.title}>Login</Text>
+            <View style={styles.form}>
+              <Text style={styles.label}>Email</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="johndoe@gmail.com"
+                keyboardType="email-address"
+                placeholderTextColor="black"
+                autoCapitalize="none"
+                autoComplete="off"
+                value={email}
+                onChangeText={setEmail}
+              />
+              {error.name === "Email" && (
+                <Text style={styles.error}>{error.error}</Text>
+              )}
+
+              <Text style={styles.label}>Password</Text>
+              <TextInput
+                style={styles.input}
+                secureTextEntry
+                placeholder="password"
+                placeholderTextColor="black"
+                autoCapitalize="none"
+                autoComplete="off"
+                value={password}
+                onChangeText={setPassword}
+              />
+              {error.name === "Password" && (
+                <Text style={styles.error}>{error.error}</Text>
+              )}
+            </View>
+            <View style={styles.button}>
+              <Button title="Login" onPress={handleSubmit} />
+            </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  innerContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  form: {
+    width: "100%",
+  },
+  label: {
+    fontSize: 15,
+    marginBottom: 5,
+  },
+  input: {
+    height: 40,
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    width: "100%",
+  },
+  error: {
+    color: "red",
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  button: {
+    width: "100%",
+    marginTop: 20,
+  },
+});
